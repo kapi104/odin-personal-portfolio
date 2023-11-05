102e22870a4dcccee104fd6c61210e85ef1d35c6
kapi104 <skorupa7979@gmail.com>
2023-11-05 21:21:58 +0100

    changed pixel values, added css for desktop view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const footer = document.querySelector('footer');
+const footerBack = document.querySelector('.footer-back')
+
+footerBack.style.height = getComputedStyle(footer).height;
+
+const setHeight = () => {
+  footerBack.style.height = getComputedStyle(footer).height;
+}
+
+window.addEventListener('resize', setHeight)
